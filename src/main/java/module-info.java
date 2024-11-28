bad4ed687c8ec378bf10a43894f539652e6a8011
salman <152821065+currentlycrafting@users.noreply.github.com>
2024-11-28 12:30:51 -0600

    Delete src directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.project_check_1_expiration {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.project_check_1_expiration to javafx.fxml;
-    exports com.example.project_check_1_expiration;
-}
